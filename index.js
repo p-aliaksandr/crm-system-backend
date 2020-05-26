f7f3de032d24f9418bed8093f4232b059fec8c2e
Sasha Prokofyev <ablehope@yandex.by>
2020-05-26 20:20:03 +0300

    created server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'working'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log('Server start...')
+})
