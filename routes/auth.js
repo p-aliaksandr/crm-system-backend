cd298828ae692c94f0d9b4ff96aeec03c22f647e
Sasha Prokofyev <ablehope@yandex.by>
2020-05-28 19:05:54 +0300

    added all routes and controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
-
 const {Router} = require('express');
 const controller = require('../controllers/auth');
 
 const router = Router();
 
-router.get('/login', controller.login);
-router.get('/register', controller.register);
+router.post('/login', controller.login);
+router.post('/register', controller.register);
 
 module.exports = router;
