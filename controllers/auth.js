63ac7138ddb0399aa601257d2cffa8322a692766
Sasha Prokofyev <ablehope@yandex.by>
2020-06-02 20:31:14 +0300

    create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 module.exports.login = function(req, res) {
   res.status(200).json({ 
     login: {
@@ -7,6 +10,29 @@ module.exports.login = function(req, res) {
   })
 }
 
-module.exports.register = function(req, res) {
-  res.status(200).json({ message: "register" })
+module.exports.register = async function(req, res) {
+  // email password
+  const candidate = await User.findOne({email: req.body.email});
+
+  if (candidate) {
+    // пользователь существует, нужно отправить ошибку
+    res.status(409).json({
+      message: 'такой email уже занят, попробуйте другой'
+    })
+  } else {
+    // нужно создать пользователя
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    })
+
+    try {
+      await user.save()
+      res.status(201).json(user)
+    } catch(e) {
+      console.log(e)
+    } 
+  }
 }
